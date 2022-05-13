9b5cfdafcc44f2fa8fc225a3b247cd6456cbf9f1
Radovan <vanhutys@gmail.com>
2022-05-13 11:52:43 +0200

    Update Nigero.lua

--- Nigero.lua
+++ Nigero.lua
@@ -5,9 +5,8 @@ end
 local r = (-(math.cos(math.deg(30))*85), -(math.sin(math.deg(30)*85)))
 local g = ((math.cos(math.deg(30))*85), -(math.sin(math.deg(30)*85)))
 local b = (0, 85)
-local blackhole
+local a
 
-m = game.mouse_2d
 screen_size = game.screen_size
 
 xmid = screen_size.width / 2 -- middle of the screen
@@ -30,19 +29,23 @@ local function on_tick()
 end
 
 local function SolveRed(x, y)
+	m = game.mouse_2d
 	rvalue = distance(m.x, m.y, r.x, r.y)
 end
 
 local function SolveGreen(x, y)
+	m = game.mouse_2d
 	gvalue = distance(m.x, m.y, g.x, g.y)
 end
 
 local function SolveBlue(x, y)
+	m = game.mouse_2d
 	bvalue = distance(m.x, m.y, b.x, b.y)
 end
 
 local function on_draw()
 	sprite:draw(xsprite, ysprite)
+	renderer:draw_rect(xsprite, ysprite, 100, -100, rvalue, gvalue, bvalue, 100)
 end
 
 client:set_event_callback("on_draw", on_draw)
