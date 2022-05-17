e54f9d85f0b7a9b4990b2e936bb749c58e758dae
Radovan <vanhutys@gmail.com>
2022-05-17 19:47:00 +0200

    Update Nigero - WIP.lua

--- Nigero - WIP.lua
+++ Nigero - WIP.lua
@@ -1,19 +1,29 @@
---if file_manager:file_exists("test.png") then
---	sprite = renderer:add_sprite("test.png", 100, 100)
---end
+if file_manager:file_exists("test.png") then
+	sprite = renderer:add_sprite("test.png", 400, 400)
+end
 
 screen_size = game.screen_size
 
 xmid = screen_size.width / 2 -- middle of the screen
 ymid = screen_size.height / 2 -- middle of the screen
 
-xsprite = xmid - 50 -- sprite size 100px
-ysprite = ymid - 50 -- sprite size 100px
+xsprite = xmid - 200 -- sprite size 100px
+ysprite = ymid - 200 -- sprite size 100px
 
-local r = {xmid, ymid+85}
-local g = {xmid -(math.cos(30)*85), ymid -(math.sin(30)*85)}
-local b = {xmid -(math.cos(30)*85), ymid -(math.sin(30)*85)}
+config = menu:add_category("Color pick")
+scale = menu:add_slider("Scale", config, 50, 1000, 180)
+
+local r = {xmid, ymid - menu:get_value(scale)}
+local g = {xmid -(math.cos(math.rad(30))) * menu:get_value(scale), ymid +(math.sin(math.rad(30))) * menu:get_value(scale)}
+local b = {xmid +(math.cos(math.rad(30))) * menu:get_value(scale), ymid +(math.sin(math.rad(30))) * menu:get_value(scale)}
 local a
+
+--console:log(tostring(r[1]))
+--console:log(tostring(r[2]))
+--console:log(tostring(g[1]))
+--console:log(tostring(g[2]))
+--console:log(tostring(b[1]))
+--console:log(tostring(b[2]))
 
 function distance(x1, y1, x2, y2) -- distance between two points
 	return math.sqrt((x2-x1)^2 + (y2-y1)^2)
@@ -22,42 +32,45 @@ end
 function on_tick()
 	m = game.mouse_2d
 	--if spellbook:key_up_down(1) and 
-	if m.x >= xsprite and m.x <= xsprite + 100 and m.y >= ysprite and m.y <= ysprite + 100 then
+	--if m.x >= xsprite and m.x <= xsprite + 100 and m.y >= ysprite and m.y <= ysprite + 100 then
 		SolveRed()
 		SolveGreen()
 		SolveBlue()
-	end
+	--end
 end
 
 function SolveRed(x, y)
-	rvalue = 255 - distance(m.x, m.y, r[1], r[2])
-			console:log(tostring(rvalue))
+	rvalue = math.floor(255 - distance(m.x, m.y, r[1], r[2]))
 	if rvalue < 0 then
 		rvalue = 0
-		console:log(tostring(rvalue))
 	end
 end
 
 function SolveGreen(x, y)
-	gvalue = distance(m.x, m.y, g[1], g[2])
-	if gvalue >= 255 then 
-		gvalue = 255
+	gvalue = math.floor(255 - distance(m.x, m.y, g[1], g[2]))
+	if gvalue < 0 then 
+		gvalue = 0
 	end
 end
 
 function SolveBlue(x, y)
-	bvalue = distance(m.x, m.y, b[1], b[2])
-	if bvalue >= 255 then
-		gvalue = 255
+	bvalue = math.floor(255 - distance(m.x, m.y, b[1], b[2]))
+	if bvalue < 0 then
+		bvalue = 0
 	end
 end
 
 function on_draw()
---	sprite:draw(xsprite, ysprite)
---	renderer:draw_rect(xsprite, ysprite, 100, 100, rvalue, gvalue, bvalue, 100)
-	renderer:draw_text(m.x + 50, m.y + 100, tostring(rvalue), 255, 255, 255, 255)
-	renderer:draw_text(m.x + 50, m.y + 200, tostring(gvalue), 255, 255, 255, 255)
-	renderer:draw_text(m.x + 50, m.y + 300, tostring(bvalue), 255, 255, 255, 255)
+	sprite:draw(xsprite, ysprite)
+	renderer:draw_rect(xsprite, ysprite, 400, 400, rvalue, gvalue, bvalue, 50)
+--	minimap:draw_circle(x, y, z, 42.5, 0, 255, 0, 255)
+--	minimap:draw_circle(x, y, z, 42.5, 0, 0, 255, 255)
+	renderer:draw_line(r[1], r[2], g[1], g[2], 1, 255, 255, 255, 255)
+	renderer:draw_line(g[1], g[2], b[1], b[2], 1, 255, 255, 255, 255)
+	renderer:draw_line(b[1], b[2], r[1], r[2], 1, 255, 255, 255, 255)
+	renderer:draw_text(r[1], r[2], "R " .. tostring(rvalue), rvalue, gvalue, bvalue, 255)
+	renderer:draw_text(g[1], g[2], "G " .. tostring(gvalue), rvalue, gvalue, bvalue, 255)
+	renderer:draw_text(b[1], b[2], "B " .. tostring(bvalue), rvalue, gvalue, bvalue, 255)
 end
 
 --]]
