7c50db46602d9d3e248b44be5106bfa20e5d5f69
Radovan <vanhutys@gmail.com>
2022-05-23 08:34:44 +0200

    Update spell_block_fix.lua

--- spell_block_fix.lua
+++ spell_block_fix.lua
@@ -1,28 +1,31 @@
 -- test
-fix = menu:add_category("Spell block bug fix")
-enable = menu:add_checkbox("Enable", fix, 0)
-hotkey = menu:add_keybinder("Hotkey which runs keyup", fix, 0x20)
-
-console:log("F5 for analysis")
-for i = 0, 65 do
-	if spellbook:get_spell_slot(i).spell_data.spell_name ~= "" then
-		console:log(tostring((i) .. " " .. spellbook:get_spell_slot(i).spell_data.spell_name .. " " .. game:is_key_down(i)))
-	end
-end
+fixcat = menu:add_category("Spell block bug fix")
+anal = menu:add_checkbox("Run analysis", fixcat, 0) -- fake button
+fix = menu:add_checkbox("Run fix", fixcat, 0) -- fake button
 
 function on_tick()
-	if menu:get_value(enable) == 1 and game:is_key_down(menu:get_value(hotkey)) then
+	if menu:get_value(anal) == 1 then
+		for i = 0, 65 do
+			if spellbook:get_spell_slot(i).spell_data.spell_name ~= "" then
+				console:log(tostring((i) .. " " .. spellbook:get_spell_slot(i).spell_data.spell_name .. " " .. game:is_key_down(i)))
+			end
+		end
+	menu:set_value(anal, 0)			
+	end
+		
+	if menu:get_value(fix) == 1 then
 		spellbook:key_up_int(0x51) -- Q
 		spellbook:key_up_int(0x57) -- W
 		spellbook:key_up_int(0x45) -- E
 		spellbook:key_up_int(0x52) -- R
 		console:log("Fixed internal Q/W/E/R")
 		for i = 0, 65 do
-			if spellbook:get_spell_slot(i).spell_data.spell_name ~= "" then
+			if spellbook:get_spell_slot(i).spell_data.spell_name ~= "" then -- ++ and game:is_key_down(i)
 				spellbook:key_up(i)
 				console:log("Fixed external spells")
 			end
 		end
+	menu:set_value(fix, 0)
 	end
 end
 
