9a3f17a99cbb4cb888fa8baa498b2c08306bc157
Radovan <vanhutys@gmail.com>
2022-06-03 06:51:59 +0200

    Update ALTF4.lua

--- ALTF4.lua
+++ ALTF4.lua
@@ -1,6 +1,6 @@
 local function on_tick()
   if game:is_key_down(0x12) and game:is_key_down(0x73) then -- reworked to right alt since the left one doesnt work
-    console:log(GAYFALL)
+    console:log(GAYFALL) -- kek
   end
 end
 
