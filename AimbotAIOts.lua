0c343178c64d03dfdfb9a6235688ac77960355a8
Radovan <vanhutys@gmail.com>
2022-06-06 14:06:49 +0200

    Update AimbotAIOts.lua

--- AimbotAIOts.lua
+++ AimbotAIOts.lua
@@ -1,8 +1,53 @@
+function GetDistanceSqr(unit, p2)
+    p2 = p2 or local_player.origin
+    p2x, p2y, p2z = p2.x, p2.y, p2.z
+    p1 = unit.origin
+    p1x, p1y, p1z = p1.x, p1.y, p1.z
+    local dx = p1x - p2x
+    local dz = (p1z or p1y) - (p2z or p2y)
+    return dx*dx + dz*dz
+end
+
+-- VECTOR MATH COPYPASTA
+--Counts minions within range
+function ml.GetMinionCount(pos, range)
+	count = 0
+    local enemies_in_range = {}
+	minions = game.minions
+	for i, minion in ipairs(minions) do
+	Range = range * range
+		if minion.is_enemy and ml.IsValid(minion) and ml.GetDistanceSqr(minion, pos) < Range then
+            table.insert(enemies_in_range, minion)
+			count = count + 1
+		end
+	end
+	return enemies_in_range, count
+end
+
+--Returns closest minion
+function ml.GetClosestMinion(pos, range)
+    local enemyMinions, _ = ml.GetMinionCount(pos, range)
+    local closestMinion = nil
+    local closestMinionDistance = 9999 
+    for i, minion in pairs(enemyMinions) do
+        if minion then
+            if minion:distance_to(mousepos) < 300 then
+                local minionDistanceToMouse = minion:distance_to(mousepos)
+                if minionDistanceToMouse < closestMinionDistance then
+                    closestMinion = minion
+                    closestMinionDistance = minionDistanceToMouse
+                end
+            end
+        end
+    end
+    return closestMinion
+end
+
 function on_draw()
 	mouse_pos = game.mouse_pos
 	
 	-- target search circle
-	renderer:draw_circle(mouse_pos.x, mouse_pos.y, mouse_pos.z, 300, 255, 255, 255, 255) -- Q + RGB A
+	renderer:draw_circle(mouse_pos.x, mouse_pos.y, mouse_pos.z, 300, 255, 255, 255, 255)
 	
 	-- line from mouse pos to target
 	if Eureka() ~= nil then
@@ -12,23 +57,16 @@ function on_draw()
 	end
 end
 
-local function Eureka(target)
+function Eureka(target)
 	--V1
 	target = selector:find_target(300, mode_cursor)
 	if target.is_valid then
 		return target
 	end
 	console:log(tostring(target))
-	
-	--V2
 	--[[
-	mouse_pos = game.mouse_pos
-	minions = game.minions
-
-	for _, v in ipairs(minions) do
-		if gameobject:distance_to(mouse_pos) < 300 then
-			
-	end
+	--V2
+	target = ml.GetClosestMinion(pos, 300)
 	console:log(tostring(target))
 	--]]
 end
