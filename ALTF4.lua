2bf06e9acb88072a811186ab5c8212fe06c2cb36
Radovan <vanhutys@gmail.com>
2022-06-07 07:21:16 +0200

    Update ALTF4.lua

--- ALTF4.lua
+++ ALTF4.lua
@@ -1,6 +1,6 @@
 -- Crash the game in case a internall spellcast bug happens
 local function on_tick()
-  if game:is_key_down(0x12) and game:is_key_down(0x73) then -- reworked to right alt since the left one doesnt work
+  if game:is_key_down(0x73) then -- F4
     console:log(GAYFALL) -- kek
   end
 end
