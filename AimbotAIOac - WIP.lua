5069884738c413363ec70fe0b8983b38faa67035
Radovan <vanhutys@gmail.com>
2022-06-07 10:47:44 +0200

    Update AimbotAIOac - WIP.lua

--- AimbotAIOac - WIP.lua
+++ AimbotAIOac - WIP.lua
@@ -1,7 +1,7 @@
 --[[
 AIMBOT AUTOCAST PLUGIN
   1. Check all targets(enemy champs) in range and calculate prediction for all of them if hitchance 80%+ then autocast spell
-  2. If they are retards = low clicks per minute(waypoints)(anti script detector), low KDA. Then lower autocast %% to 60%
+  2. If they are retards = low clicks per minute(anti script detector), low KDA. Then lower autocast %% to 60%
   3. If they are living gods == cheaters with evade only cast at 95%+
   V2 -- For circular spells also output enemy count
 CREDITS: Benwalker aka stoneb2 
@@ -26,10 +26,6 @@ local input = {
   hitbox = true,
 }
 
-waypoints = menu:add_category("Script Detector")
-cheater_limit = menu:add_slider("Cheater Clicks per Second Limit", waypoints, 5, 50, 10, "The number of clicks per second to be labeled by the code as a scripter")
-time_limit_slider = menu:add_slider("Average Time Length", waypoints, 1, 20, 5, "Time interval to collect waypoints. Works best at 5 seconds, do not recommend changing.")
-
 local function GetEnemyHeroes()
 local _EnemyHeroes = {}
 players = game.players
@@ -41,51 +37,10 @@ players = game.players
 	return _EnemyHeroes
 end
 
-cheater_list = {}
-for i, player in ipairs(game.players) do
-    cheater_list[player.object_id] = false
-end
-
-tracker = {}
-for i, player in ipairs(game.players) do
-    tracker[player.object_id] = {
-        path = {},
-        time = {}
-    }
-end
-
-averages = {}
-local function on_new_path(obj, path)
-    if obj.is_valid then
-        for i, player in ipairs(game.players) do
-            if player.object_id == obj.object_id then
-                table.insert(tracker[obj.object_id].path, path)
-                table.insert(tracker[obj.object_id].time, game.game_time)
-            end
-        end
-    end
-end
-
 local function getdynamichitchance() -- ±15%
-	for i, player in pairs(tracker) do
-		for j, time in pairs(player) do
-			for k, actual_time in pairs(time) do
-				if type(actual_time) == "number" then
-					if actual_time <= game.game_time - menu:get_value(time_limit_slider) then
-						table.remove(tracker[i].path, k)
-						table.remove(tracker[i].time, k)
-					end
-				end
-			end
-		end
-	end
-	for i, player in pairs(tracker) do
-		averages[i] = #player.time / menu:get_value(time_limit_slider)
-		if averages[i] > tonumber(menu:get_value(cheater_limit)) and not cheater_list[i] and i ~= local_player.object_id then
-			cheater_list[i] = true
-			champ_name = game:get_object(i).champ_name
-			return 0.15 
-		end
+	-- Ben script detector reworked
+	-- If unit.name == champ.name on cheater list return 0.15
+	else return 0
 	end
 end
 
