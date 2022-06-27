3a8be7e7071cfec8f6b381710e5d074929df18f9
Que <8728328+QuePast@users.noreply.github.com>
2022-06-27 08:47:55 +0200

    Delete ALTF4.lua
    Deleted, not needed

--- ALTF4.lua
+++
@@ -1,8 +0,0 @@
--- Crash the game in case a internall spellcast bug happens
-local function on_tick()
-  if game:is_key_down(0x73) then -- F4
-    console:log(GAYFALL) -- kek
-  end
-end
-
-client:set_event_callback("on_tick", on_tick)
