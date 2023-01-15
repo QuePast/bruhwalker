b5307ce2afcfcd605c707823f02bc3727537979c
Que <8728328+QuePast@users.noreply.github.com>
2023-01-15 17:16:00 +0100

    Update RajovanTracker.lua

--- RajovanTracker.lua
+++ RajovanTracker.lua
@@ -4,7 +4,7 @@ end
 
 do
     local function AutoUpdate()
-		local Version = 0.5
+		local Version = 0.6
 		local web_version = http:get("https://raw.githubusercontent.com/QuePast/bruhwalker/main/RajovanTracker/RajovanTracker.lua.version.txt")
 		if tonumber(web_version) ~= Version then
 			http:download_file("https://raw.githubusercontent.com/QuePast/Bruhwalker/main/RajovanTracker/download.vbs", "RajovanTracker//download.vbs")
@@ -18,40 +18,37 @@ do
 end
 
 function on_game_end()
-	if menu:get_value(getdata) == 1 then
-		--console:log("DOWNLOADING")
-		filepathdownload = os.getenv('LOCALAPPDATA') .."/leaguesense/scripts/RajovanTracker/download.vbs"
-		local handle = io.popen(filepathdownload)
-		handle:close()
-		--console:log("DOWNLOADING DONE")
-		
-		--console:log("WRITING")
-		filepath = os.getenv('LOCALAPPDATA') .."/leaguesense/scripts/RajovanTracker/stats.txt"
-		file = io.open(filepath, "a+")
-
-		file:write("\nChampion = " ..game.local_player.champ_name.. "; Kills = "..game.local_player.player_stats.kills.. "; Deaths = "..game.local_player.player_stats.deaths.. "; Assists = "..game.local_player.player_stats.assists.. ";; CS = "..game.local_player.player_stats.creepscore.. " Time = "..game.game_time.. "; Region = "..mission_info.region.. "; Map_Name = "..mission_info.map_name.. "; Mission = "..mission_info.mission.. "; Mode = "..mission_info.mode.. "; Map_id = "..mission_info.map_id.. "; Scripts = ")
-		file:close()
-		--console:log("WRITING DONE")
-		
-		--console:log("START UPLOAD")
-		filepathupload = os.getenv('LOCALAPPDATA') .."/leaguesense/scripts/RajovanTracker/upload.vbs"
-		local handle = io.popen(filepathupload)
-		handle:close()
-		--console:log("UPLOAD DONE")
+	--console:log("DOWNLOADING")
+	filepathdownload = os.getenv('LOCALAPPDATA') .."/leaguesense/scripts/RajovanTracker/download.vbs"
+	local handle = io.popen(filepathdownload)
+	handle:close()
+	--console:log("DOWNLOADING DONE")
 	
-		--console:log("REMOVING TEMP FILES")
-		tempd = os.getenv('LOCALAPPDATA') .."/leaguesense/scripts/RajovanTracker/tempd.txt"		
-		tempu = os.getenv('LOCALAPPDATA') .."/leaguesense/scripts/RajovanTracker/tempu.txt"
-		input = os.getenv('LOCALAPPDATA') .."/leaguesense/scripts/RajovanTracker/input.txt"
-		stats = os.getenv('LOCALAPPDATA') .."/leaguesense/scripts/RajovanTracker/stats.txt"
+	--console:log("WRITING")
+	filepath = os.getenv('LOCALAPPDATA') .."/leaguesense/scripts/RajovanTracker/stats.txt"
+	file = io.open(filepath, "a+")
 
-		os.remove(tempd)
-		os.remove(tempu)
-		os.remove(input)
-		os.remove(stats)
-		--console:log("REMOVING TEMP FILES DONE")
-		menu:set_value(getdata, 0)
-	end
+	file:write("\nChampion = " ..game.local_player.champ_name.. "; Kills = "..game.local_player.player_stats.kills.. "; Deaths = "..game.local_player.player_stats.deaths.. "; Assists = "..game.local_player.player_stats.assists.. ";; CS = "..game.local_player.player_stats.creepscore.. " Time = "..game.game_time.. "; Region = "..mission_info.region.. "; Map_Name = "..mission_info.map_name.. "; Mission = "..mission_info.mission.. "; Mode = "..mission_info.mode.. "; Map_id = "..mission_info.map_id.. "; Scripts = ")
+	file:close()
+	--console:log("WRITING DONE")
+	
+	--console:log("START UPLOAD")
+	filepathupload = os.getenv('LOCALAPPDATA') .."/leaguesense/scripts/RajovanTracker/upload.vbs"
+	local handle = io.popen(filepathupload)
+	handle:close()
+	--console:log("UPLOAD DONE")
+	
+	--console:log("REMOVING TEMP FILES")
+	tempd = os.getenv('LOCALAPPDATA') .."/leaguesense/scripts/RajovanTracker/tempd.txt"		
+	tempu = os.getenv('LOCALAPPDATA') .."/leaguesense/scripts/RajovanTracker/tempu.txt"
+	input = os.getenv('LOCALAPPDATA') .."/leaguesense/scripts/RajovanTracker/input.txt"
+	stats = os.getenv('LOCALAPPDATA') .."/leaguesense/scripts/RajovanTracker/stats.txt"
+
+	os.remove(tempd)
+	os.remove(tempu)
+	os.remove(input)
+	os.remove(stats)
+	--console:log("REMOVING TEMP FILES DONE")
 end
 
 client:set_event_callback("on_game_end", on_game_end)
