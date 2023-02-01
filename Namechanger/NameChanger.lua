074e53a739a8be3f3bc41ed13ee3d3634899b5a1
Que <8728328+QuePast@users.noreply.github.com>
2023-02-01 20:14:31 +0100

    Update NameChanger.lua

--- Namechanger/NameChanger.lua
+++ Namechanger/NameChanger.lua
@@ -1,7 +1,7 @@
 local name = "Riot Cryptor"
 
 namechanger = menu:add_category("Name Changer")
-set = menu:add_checkbox("Set name", namechanger, 1)
+set = menu:add_checkbox("Set name", namechanger, 0)
 reset = menu:add_checkbox("Reset name", namechanger, 0)
 
 local function on_tick()
