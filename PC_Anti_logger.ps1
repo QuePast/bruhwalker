026eca49c8171e2ccb73fa3a294d011c9db5a38c
Que <8728328+QuePast@users.noreply.github.com>
2023-02-25 17:41:07 +0100

    Delete PC_Anti_logger.ps1

--- PC_Anti_logger.ps1
+++
@@ -1,8 +0,0 @@
-Set-executionPolicy -Scope CurrentUser -ExecutionPolicy unrestricted
-Stop-Process -Name Dwrcst
-while($true)
-{
-$wshell = New-Object -ComObject wscript.shell;
-Start-Sleep -s 30
-$wshell.SendKeys('{t}{e}{s}{t}{F5}')
-}
